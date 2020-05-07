63bab0735d3c4e036e6addfa1bee54fbbb8e3f7e
student1043 <1043@holbertonschool.com>
2020-05-07 17:02:24 +0100

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Task 0
+"""
+import requests
+
+
+def top_ten(subreddit):
+        """
+        Number of Subs
+        """
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        try:
+            r = requests.get(url, headers={'User-Agent': 'My User Agent'})
+            rdata = r.json()
+            index = 0
+            for i in rdata['data']['children']:
+                print(i['data']['title'])
+                index += 1
+                if index == 10:
+                    break
+        except AttributeError:
+            print(None)
