8ad13088c9e1c0390e682bd4086f4b167953384b
student1043 <1043@holbertonschool.com>
2020-05-07 14:47:58 +0100

    Fixing TASK 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-import requests
+"""
+Task 0
+"""
 import json
+import requests
 
 
 def number_of_subscribers(subreddit):
+        """ 
+        Number of Subs 
+        """
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         r = requests.get(url, headers={'User-Agent': 'My User Agent'})
         rdata = r.json()
