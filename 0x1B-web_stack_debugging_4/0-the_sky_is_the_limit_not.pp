e068f53591a8f4516ec6e6b9bbcfef3fa2e1931f
student1043 <1043@holbertonschool.com>
2020-07-05 13:43:46 +0100

    Fixing issue

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # benchmarking Apache Server
 exec {'benchmarking':
 command => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
-path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
