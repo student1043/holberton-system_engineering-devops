8bb154449357f620db8c45fe201c9591cff2b501
student1043 <1043@holbertonschool.com>
2020-07-05 13:59:23 +0100

    Hot Fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,5 @@
 # Fixing the login without errors
-exec { 'errorfix:
-command => '/usr/bin/env sed -i "s/holberton/" /etc/security/limits.conf',
+exec { 'errorfix':
+command  => '/usr/bin/env sed -i "s/holberton/" /etc/security/limits.conf',
+provider => shell,
 }
